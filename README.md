eb7a03abbd2d3ef805bb24782d8f9ac3cf4c3af2
Yousra-Amin <83617096+Yousra-Amin@users.noreply.github.com>
2026-04-17 05:37:42 -0400

    finished code and added it to jupyter notebook

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Smart-Rank-ML
+
+Link to `features_dataset.csv`: https://drive.google.com/drive/folders/152xWKjEohgoFphAQ8LrLtulna7lgCBH5?usp=sharing
+
--- ranking_scripts/6_click_through_rate.py
+++
@@ -1,9 +0,0 @@
-from config import df
-
-# Compute CTR = clicks / impressions (if available in dataset) 
-# For MS MARCO, use relevance as a proxy if raw clicks unavailable 
-if 'click_count' in df.columns and 'impression_count' in df.columns: 
-    df['ctr'] = df['click_count'] / (df['impression_count'] + 1e-9) 
-else: 
-# Proxy: proportion of relevant docs per query 
-    df['ctr'] = df.groupby('qid')['relevance'].transform('mean')
